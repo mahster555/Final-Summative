1ae756eb3f573735f9ae182a0f700d5899e99b03
mahster555 <81257692+mahster555@users.noreply.github.com>
2021-05-28 11:10:26 -0400

    Spacing it out and Formatted it better
    I made it so it would space out after each section of calculations so it can clearly be seen instead of all clumped together, and I made the program format a little better. I also tested the program with it in methods, and works fine

--- FinalSummative.java
+++ FinalSummative.java
@@ -30,6 +30,7 @@ class FinalSummative {
         // Used as the main number throughout the calculations.
         System.out.println("Enter your income salary:");
         int c = in.nextInt(); 
+        System.out.println();
         
         printCPP(a,b,c);
         printEI(c,d);
@@ -37,44 +38,40 @@ class FinalSummative {
         printPIT(c,f);
         printTYD(a,b,c,d);
     }
-       
-        public static void printCPP(int a,double b,int c){
+    public static void printCPP(int a,double b,int c){
         // Finds the useres CPP (Canada Pension Plan) based on their income salary and Ontario's deduction numbers.
         System.out.println("Calculating Canada Pension Plan");
         System.out.println(c + " - " + a + " = " + Integer.sum(a, c));  
         System.out.println(Integer.sum(a,c) + " x " + b);
         System.out.println(("Total CPP: ") + (Math.round(Integer.sum(a,c) * b)*10.0)/10.0);
-        }
-
-
-        public static void printEI(int c,double d){
+        System.out.println();
+    }
+    public static void printEI(int c,double d){
         // Finds the EI (Employment Insurance) based on their income salary and Ontario's given percentage.
         System.out.println("Calculating Employmemt Insurance");
         System.out.println(c + " x " + d);
         System.out.println("Total EI: " + (Math.round(c * d)*10.0)/10.0);
-        }
-
-        public static void printFIT(int c,double e){
+        System.out.println();
+    }
+    public static void printFIT(int c,double e){
         // Finds the Federal Income Tax based on the gross income.
         System.out.println("Calculating Federal Income Tax");
         System.out.println(c + " x " + e);
         System.out.println("Total Federal Income Tax: " + (Math.round(c * e)*10.0)/10.0);
-
-        }
-
-        public static void printPIT(int c,double f){
+        System.out.println();
+    }
+    public static void printPIT(int c,double f){
         // Finds the Pronvicial Income Tax based on the gross income.
         System.out.println("Calculating Provinvial Income Tax");
         System.out.println(c + " x " + f);
         System.out.println("Total Provincial Income Tax: " + (Math.round(c * f)*10.0)/10.0);
-        }
-
-
-        public static void printTYD(int a,double b,int c, double d){
+        System.out.println();
+    }
+    public static void printTYD(int a,double b,int c, double d){
         // Calculating Total Yearly Deductions.
         System.out.println("Calculating Total Yearly Deductions");
         System.out.println((Math.round(Integer.sum(a,c) * b)*10.0)/10.0 + " + " + (Math.round(c * d)*10.0)/10.0);
         System.out.println("Total Yearly Deductions: " + (Math.round(Integer.sum(a,c) * b + c * d)*10.0)/10.0);
-
+        System.out.println();
     }
 }
