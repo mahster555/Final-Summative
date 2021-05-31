53161a09079322302bc57e5b872076d75eaa3783
mahster555 <81257692+mahster555@users.noreply.github.com>
2021-05-30 22:50:29 -0400

    Made some more adjustments
    I made some more adjustments by adding a $ sign for every calculation, also added some comments for the calculations saying it is rounded to the whole number, and changed a line indicating to make sure their salary isn't a decimal value.  But overall it works fine and tested it to make sure. (p.s. commented if wanted to make a while statement to make user re-input if input is an error)

--- FinalSummative.java
+++ FinalSummative.java
@@ -27,8 +27,9 @@ class FinalSummative {
         System.out.println("What is your occupation?");
         in.nextLine();
 
-        // Used as the main number throughout the calculations.
-        System.out.println("Enter your income salary:");
+        // Used as the main number throughout the calculations (input as whole number or results in error).
+        // Can make a while statement if you would like
+        System.out.println("Enter your income salary (to the whole number, if not, reults in an error):");
         int c = in.nextInt(); 
         System.out.println();
         
@@ -38,58 +39,58 @@ class FinalSummative {
         printPIT(c,f);
         printTYD(a,b,c,d);
     }
-    // Finds the useres CPP (Canada Pension Plan) based on their income salary and Ontario's deduction numbers.
+    // Finds the useres CPP (Canada Pension Plan) based on their income salary and Ontario's deduction numbers (rounded to the whole number).
     public static void printCPP(int a, double b, int c){
         System.out.println("Calculating Canada Pension Plan");
-        System.out.println(c + " - " + a + " = " + Integer.sum(a, c));  
+        System.out.println("$" + c + " - " + a + " = " + Integer.sum(a, c));  
         System.out.println(Integer.sum(a,c) + " x " + b);
-        System.out.println("Total CPP: " + calculateCPP(a,b,c));
+        System.out.println("Total CPP: $" + calculateCPP(a,b,c));
         System.out.println();
     }
-     // Finds the EI (Employment Insurance) based on their income salary and Ontario's given percentage.
+     // Finds the EI (Employment Insurance) based on their income salary and Ontario's given percentage (rounded to the whole number).
     public static void printEI(int c, double d){
         System.out.println("Calculating Employmemt Insurance");
-        System.out.println(c + " x " + d);
-        System.out.println("Total EI: " + calculateEI(c,d));
+        System.out.println("$" + c + " x " + d);
+        System.out.println("Total EI: $" + calculateEI(c,d));
         System.out.println();
     }
-    // Finds the Federal Income Tax based on the gross income.
+    // Finds the Federal Income Tax based on the gross income (rounded to the whole number).
     public static void printFIT(int c, double e){
         // Finds the Federal Income Tax based on the gross income.
         System.out.println("Calculating Federal Income Tax");
-        System.out.println(c + " x " + e);
-        System.out.println("Total Federal Income Tax: " + calculateFIT(c,e));
+        System.out.println("$" + c + " x " + e);
+        System.out.println("Total Federal Income Tax: $" + calculateFIT(c,e));
         System.out.println();
     }
-    // Finds the Pronvicial Income Tax based on the gross income.
+    // Finds the Pronvicial Income Tax based on the gross income (rounded to the whole number).
     public static void printPIT(int c, double f){
         System.out.println("Calculating Provinvial Income Tax");
-        System.out.println(c + " x " + f);
-        System.out.println("Total Provincial Income Tax: " + calculatePIT (c,f));
+        System.out.println("$" + c + " x " + f);
+        System.out.println("Total Provincial Income Tax: $" + calculatePIT (c,f));
         System.out.println();
     }
-    // Calculating Total Yearly Deductions based off of the calulations above.
+    // Calculating Total Yearly Deductions based off of the calulations above (rounded to the whole number).
     public static void printTYD(int a, double b, int c, double d){
         System.out.println("Calculating Total Yearly Deductions");
-        System.out.println((Math.round(Integer.sum(a,c) * b)*10.0)/10.0 + " + " + (Math.round(c * d)*10.0)/10.0);
-        System.out.println("Total Yearly Deductions: " + calculateTYD(a,b,c,d));
+        System.out.println("$" + (Math.round(Integer.sum(a,c) * b)*100)/100 + " + $" + (Math.round(c * d)*100)/100);
+        System.out.println("Total Yearly Deductions: $" + calculateTYD(a,b,c,d));
         System.out.println();
     }
     // Function calls used for table. 
     public static double calculateCPP(int a, double b, int c){
-        return (Math.round(Integer.sum(a,c) * b)*10.0)/10.0;
+        return (Math.round(Integer.sum(a,c) * b)*100)/100;
     }
     public static double calculateEI(int c, double d){
-        return (Math.round(c * d)*10.0)/10.0;
+        return (Math.round(c * d)*100)/100;
     }
     public static double calculateFIT(int c, double e){
-        return (Math.round(c * e)*10.0)/10.0;
+        return (Math.round(c * e)*100)/100;
     }
     public static double calculatePIT(int c, double f){
-        return (Math.round(c * f)*10.0)/10.0;
+        return (Math.round(c * f)*100)/100;
     }
     public static double calculateTYD(int a, double b, int c, double d){
-        return (Math.round(Integer.sum(a,c) * b + c * d)*10.0)/10.0;
+        return (Math.round(Integer.sum(a,c) * b + c * d)*100)/100;
     }
     
 }
